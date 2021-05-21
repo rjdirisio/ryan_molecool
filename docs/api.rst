0c38b166a12f5bf819db11cd5341a6f8202346da
Ryan DiRisio <rjdiri@uw.edu>
2021-05-21 12:57:42 -0700

    Initial commit after CMS Cookiecutter creation, version 1.5

---
+++ docs/api.rst
@@ -0,0 +1,7 @@
+API Documentation
+=================
+
+.. autosummary::
+   :toctree: autosummary
+
+   ryan_molecool.canvas
---
+++ ryan_molecool/tests/test_ryan_molecool.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the ryan_molecool package.
+"""
+
+# Import package, test suite, and other packages as needed
+import ryan_molecool
+import pytest
+import sys
+
+def test_ryan_molecool_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "ryan_molecool" in sys.modules
